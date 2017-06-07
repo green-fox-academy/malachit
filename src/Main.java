bbef6f2ca1b230b6e9de27870b42e1e3b899cbf7
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-06-07 14:53:56 +0200

    a2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 public class Main {
 
+  public static String NAME = "zandraa";
+
   public static void main(String[] args) {
-    greet();
+    greet(NAME);
   }
 
-  public static void greet() {
-    System.out.println("zandraa");
+  public static void greet(String name) {
+    System.out.println(name);
   }
 }
