5c3bfd0c3782b4039148ab70dfc5d8c7b8a80fb1
btekse <btekse@gmail.com>
2017-06-07 14:42:07 +0200

    c1

---
+++ src/Cheer.java
@@ -0,0 +1,16 @@
+
+public class Cheer {
+  private String cheerMessage;
+
+    public Cheer() {
+      }
+
+  public Cheer(String cheerMessage) {
+    this.cheerMessage = "Malachit";
+  }
+
+  public void cheerMessage() {
+    System.out.println(this.cheerMessage);
+  }
+
+}
