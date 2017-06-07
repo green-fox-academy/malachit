1bd550ee371ffb6e25f8a9515eb4338e6e6fdf9b
btekse <btekse@gmail.com>
2017-06-07 16:34:58 +0200

    c3.3

--- src/Cheer.java
+++ src/Cheer.java
@@ -1,12 +1,21 @@
 
 public class Cheer {
   private String cheerMessage;
+  private String chx;
 
-    public Cheer() {
-      }
+  public Cheer() {
+  }
 
   public Cheer(String cheerMessage) {
     this.cheerMessage = "Malachit";
+  }
+
+  public String getChx() {
+    return chx;
+  }
+
+  public void setChx(String chx) {
+    this.chx = chx;
   }
 
   public void cheerMessage() {
